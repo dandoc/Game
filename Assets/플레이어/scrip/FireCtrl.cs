ce0fe5f4fd2e15bad4a4c746338a906b9e4d7466
dandoc <dandoc3370@gmail.com>
2019-09-10 12:28:50 +0900

    l

--- Assets/플레이어/scrip/FireCtrl.cs
+++ Assets/플레이어/scrip/FireCtrl.cs
@@ -7,41 +7,40 @@ public class FireCtrl : MonoBehaviour
     public GameObject bullet;
     public Transform firePos;
 
-    public float del_time = 0.001f;
+    public float del_time;
+    private bool flag;
 
     private float count_time = 0f;
 
-   
     void Start()
     {
-
+        flag = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.X) && !flag)
         {
             Fire();
-            count_time += Time.deltaTime;
-  
+            flag = true;
         }
 
-        if(count_time >= del_time)
+        if(flag == true)
         {
-            count_time = 0f;
-        
+            count_time += Time.deltaTime;
+            if (count_time >= del_time)
+            {
+                count_time = 0;
+                flag = false;
+                
+            }
         }
     }
 
     void Fire()
     {
-        if(count_time == 0f)
-        {
-
-            Instantiate(bullet, firePos.position, firePos.rotation);
-
-        }
+        Instantiate(bullet, firePos.position, firePos.rotation);
 
     }
 }
